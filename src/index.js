892505c8e922d8bce2f9c687e2e4718d8b3d9397
minhtuanha98 <minhtuanha98@gmail.com>
2020-11-02 14:56:35 +0700

    fix sidebar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 import store from './store/store';
 import { Provider } from "react-redux";
@@ -15,18 +13,5 @@ ReactDOM.render(
     </React.StrictMode>
   </Provider>,
  
-  document.getElementById('root')
-=======
-=======
->>>>>>> 47994254dc8ad22144452bb7d8ba0baffb2d09ca
-import  { Provider } from 'react-redux';
-import store from './store/store';
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>,
-  document.getElementById('root'),
->>>>>>> 4799425... fix SkillTable
+document.getElementById('root')
 );
