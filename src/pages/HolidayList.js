6dd92c9dac4b01f2ed2713c70ae9ca5e42747900
Duc Nguyen Van <ducnv@nal.vn>
2020-11-02 13:21:25 +0700

    fix holiday list page

--- src/pages/HolidayList.js
+++ src/pages/HolidayList.js
@@ -4,14 +4,18 @@ import HolidaySearchBox from '../components/HolidaySearchBox'
 import HolidayTable from '../components/HolidayTable';
 import SideBar from '../components/SideBar'
 import '../css/HolidayList.css';
+import { useSelector } from 'react-redux';
 
 function HolidayList() {
+    //get state from redux
+    const isSideBarOpen = useSelector(state => state.sideBarReducer.isSideBarOpen);
+
     return (
         <div>
             <Header />
             <SideBar />
 
-            <div className="holiday-list-content">
+            <div style={{marginLeft: isSideBarOpen? "230px" : "56px"}} className="holiday-list-content">
                 <HolidaySearchBox />
                 <HolidayTable />
             </div>
