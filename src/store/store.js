f8caa4f240e46747598f2c1b0ded6f0458df7d1a
minhtuanha98 <minhtuanha98@gmail.com>
2020-11-02 13:31:29 +0700

    fix siderBar

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+store.js
+
+import { createStore, combineReducers } from "redux";
+import sideBarReducer from '../reducers/sideBarReducer';
+
+const reducer = combineReducers({
+  sideBarReducer: sideBarReducer
+});
+
+const store = createStore(reducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+export default store;
