069828c52250d272e1fbf20899aa83612b306eeb
QuyHuy <baoquyhuy@gmail.com>
2020-11-02 09:56:51 +0700

    fix conflict

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+// import './App.css';
+// import 'antd/dist/antd.css';
+// import DarkLayer from './components/DarkLayer';
+// import UserList from './pages/UserList';
+// import Login from './pages/Login';
+
+// import UserRegist from './pages/UserRegist';
+// import HolidayList from './pages/HolidayList';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Switch>
+//           <Route exact path="/" component={Login} />
+
+//           <Route exact path="/userlist" component={UserList} />
+          
+//           <Route exact path="/userregist" component={UserRegist} />
+           
+//           <Route exact path="/holidaylist" component={HolidayList} /> 
+//         </Switch>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
@@ -8,8 +40,8 @@ import UserList from './pages/UserList';
 import Login from './pages/Login';
 
 import UserRegist from './pages/UserRegist';
-import SkillRegist from './pages/SkillRegist';
 import HolidayList from './pages/HolidayList';
+import SkillRegist from './pages/SkillRegist';
 
 function App() {
   return (
@@ -17,12 +49,12 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Login} />
-          
-          <Route exact path="/userlist" component={UserList} />
+
+          <Route exact path="/users" component={UserList} />
           
           <Route exact path="/userregist" component={UserRegist} />
            
-          <Route exact path="/holidaylist" component={HolidayList} /> 
+          <Route exact path="/holiday" component={HolidayList} /> 
 
           <Route exact path="/skillregist" component={SkillRegist} /> 
         </Switch>
