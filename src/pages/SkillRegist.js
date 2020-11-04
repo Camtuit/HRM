fb0f1a025eb2302f194f4feb2b2d16a194f711b9
QuyHuy <baoquyhuy@gmail.com>
2020-11-04 10:10:32 +0700

    fix code convention

--- src/pages/SkillRegist.js
+++ src/pages/SkillRegist.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Header from "../components/Header";
-import SideBar from "../components/SideBar";
-import SkillRegistInput from "../components/SkillRegistInput";
-import "../css/SkillRegist.css";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Header from '../layouts/Header';
+import SideBar from '../layouts/SideBar';
+import SkillRegistInput from '../components/Skill/SkillRegistInput';
+import '../css/SkillRegist.css';
 
 function SkillRegist() {
   const isSideBarOpen = useSelector(
-    (state) => state.sideBarReducer.isSideBarOpen
+    (state) => state.sideBarReducer.isSideBarOpen,
   );
 
   return (
@@ -15,7 +15,7 @@ function SkillRegist() {
       <Header />
       <SideBar />
       <div
-        style={{ marginLeft: isSideBarOpen ? "230px" : "56px" }}
+        style={{ marginLeft: isSideBarOpen ? '230px' : '56px' }}
         className="skill-registration-content"
       >
         <SkillRegistInput />
