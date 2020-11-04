65e95b41037b407570089fb21664d6fb51ce5221
QuyHuy <baoquyhuy@gmail.com>
2020-11-04 09:09:35 +0700

    fix code convention

--- src/pages/HolidayRegist.js
+++ src/pages/HolidayRegist.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Header from '../components/Header';
-import HolidayRegistInput from '../components/HolidayRegistInput';
-import SideBar from '../components/SideBar';
-import '../css/HolidayRegist.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../components/Header";
+import HolidayRegistInput from "../components/HolidayRegistInput";
+import SideBar from "../components/SideBar";
+import "../css/HolidayRegist.css";
 
 function HolidayRegist() {
-    const isSideBarOpen = useSelector(state => state.sideBarReducer.isSideBarOpen);
+  const isSideBarOpen = useSelector(
+    (state) => state.sideBarReducer.isSideBarOpen
+  );
 
-    return (
-      <>
-        <Header />
-        <SideBar />
-        <div style={{marginLeft: isSideBarOpen? "230px" : "56px"}} className="holiday-registration-content">
-          <HolidayRegistInput />
-        </div>
-      </>
-    )
+  return (
+    <>
+      <Header />
+      <SideBar />
+      <div
+        style={{ marginLeft: isSideBarOpen ? "230px" : "56px" }}
+        className="holiday-registration-content"
+      >
+        <HolidayRegistInput />
+      </div>
+    </>
+  );
 }
 
 export default HolidayRegist;
--- src/pages/SkillRegist.js
+++ src/pages/SkillRegist.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import Header from '../components/Header'
-import SideBar from '../components/SideBar';
-import SkillRegistInput from '../components/SkillRegistInput';
-import '../css/SkillRegist.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
+import SkillRegistInput from "../components/SkillRegistInput";
+import "../css/SkillRegist.css";
 
 function SkillRegist() {
-  const isSideBarOpen = useSelector(state => state.sideBarReducer.isSideBarOpen);
+  const isSideBarOpen = useSelector(
+    (state) => state.sideBarReducer.isSideBarOpen
+  );
 
   return (
     <>
       <Header />
       <SideBar />
-      <div style={{marginLeft: isSideBarOpen? "230px" : "56px"}} className="skill-registration-content">
+      <div
+        style={{ marginLeft: isSideBarOpen ? "230px" : "56px" }}
+        className="skill-registration-content"
+      >
         <SkillRegistInput />
       </div>
     </>
