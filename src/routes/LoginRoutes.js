df6b81942bb354fdc18f6783d7d6bbb2bddb112b
Thiem Hoang Cao <thiemhc@nal.vn>
2020-11-04 13:57:30 +0700

    Configure hot reloading

--- src/routes/LoginRoutes.js
+++ src/routes/LoginRoutes.js
@@ -3,5 +3,5 @@ import { Route } from 'react-router-dom';
 import Login from '../pages/Login';
 
 export default function HomeRoutes() {
-  return <Route path="/login" component={Login}></Route>;
+  return <Route path="/login" component={Login} />;
 }
