37a430da9fe4349e0d024ef5a2776e1e4df6f142
Thiem Hoang Cao <thiemhc@nal.vn>
2020-11-05 20:04:39 +0700

    Fix conflict pull

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Avatar } from 'antd';
 
 function Header() {
   const handleToggleSidebar = () => {
     const [sideBar] = document.getElementsByClassName('side-bar');
-    if (sideBar.classList.contains('toggled'))
+    const [content] = document.getElementsByClassName('wrapper-content');
+    if (sideBar && sideBar.classList.contains('toggled'))
       sideBar.classList.remove('toggled');
     else sideBar.classList.add('toggled');
+    if (content && content.classList.contains('toggled'))
+      content.classList.remove('toggled');
+    else content && content.classList.add('toggled');
   };
 
   return (
