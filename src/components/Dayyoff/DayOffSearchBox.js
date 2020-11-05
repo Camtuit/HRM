8ba022fd4b4d0839b488d7f0b9cd2fde1eecbc5f
QuyHuy <baoquyhuy@gmail.com>
2020-11-05 11:12:47 +0700

    [HRM-87] create constant file to fix text and validation file to fix validate form

--- src/components/Dayyoff/DayOffSearchBox.js
+++ src/components/Dayyoff/DayOffSearchBox.js
@@ -1,19 +1,8 @@
 import { Button, DatePicker, Form, Input, Select } from 'antd';
 import React, { useState } from 'react';
-import '../../css/DayOffSearchBox.css';
 
-const LABEL_COL = 2;
-const WRAPPER_COL = 10;
-const LAYOUT = 'horizontal';
-const LABEL = {
-  DAY_OFF: 'Day off',
-  NAME: 'Name',
-};
-const BUTTON = {
-  CANCEL: 'Cancel',
-  SEARCH: 'Search',
-};
-const FORMAT_DATE = 'DD-MM-YYYY';
+import constant from '../../constants/constants';
+import '../../css/DayOffSearchBox.css';
 
 function DayOffSearchBox() {
   const [componentSize, setComponentSize] = useState('default');
@@ -24,12 +13,12 @@ function DayOffSearchBox() {
     <div className="dayoff-search-box">
       <Form
         labelCol={{
-          span: LABEL_COL,
+          span: constant.LABEL_COL.LABEL_COL_2,
         }}
         wrapperCol={{
-          span: WRAPPER_COL,
+          span: constant.WRAPPER_COL.WRAPPER_COL_10,
         }}
-        layout={{ LAYOUT }}
+        layout={constant.LAYOUT.HORIZONTAL}
         initialValues={{
           size: componentSize,
         }}
@@ -37,29 +26,45 @@ function DayOffSearchBox() {
         size={componentSize}
       >
         <div className="dayoff-search-box-input">
-          <Form.Item label={LABEL.NAME}>
+          <Form.Item label={constant.LABEL.NAME}>
             <Input />
           </Form.Item>
 
-          <Form.Item label={LABEL.DAY_OFF}>
-            <DatePicker placeholder={FORMAT_DATE} format={FORMAT_DATE} />
+          <Form.Item label={constant.LABEL.DAY_OFF}>
+            <DatePicker
+              placeholder={constant.FORMAT_DATE}
+              format={constant.FORMAT_DATE}
+            />
             <span> to </span>
-            <DatePicker placeholder={FORMAT_DATE} format={FORMAT_DATE} />
+            <DatePicker
+              placeholder={constant.FORMAT_DATE}
+              format={constant.FORMAT_DATE}
+            />
           </Form.Item>
 
           <Form.Item label="Quick choose">
             <Select>
-              <Select.Option value="demo">Today</Select.Option>
-              <Select.Option value="demo">This week</Select.Option>
-              <Select.Option value="demo">Last wwek</Select.Option>
-              <Select.Option value="demo">This month</Select.Option>
-              <Select.Option value="demo">Last month</Select.Option>
+              <Select.Option value="demo">
+                {constant.QUICK_CHOOSE.TODAY}
+              </Select.Option>
+              <Select.Option value="demo">
+                {constant.QUICK_CHOOSE.THIS_WEEK}
+              </Select.Option>
+              <Select.Option value="demo">
+                {constant.QUICK_CHOOSE.LAST_WEEK}
+              </Select.Option>
+              <Select.Option value="demo">
+                {constant.QUICK_CHOOSE.THIS_MONTH}
+              </Select.Option>
+              <Select.Option value="demo">
+                {constant.QUICK_CHOOSE.LAST_MONTH}
+              </Select.Option>
             </Select>
           </Form.Item>
         </div>
         <div className="dayoff-search-box-button">
-          <Button>{BUTTON.CANCEL}</Button>
-          <Button type="primary">{BUTTON.SEARCH}</Button>
+          <Button>{constant.BUTTON.CANCEL}</Button>
+          <Button type="primary">{constant.BUTTON.SEARCH}</Button>
         </div>
       </Form>
     </div>
