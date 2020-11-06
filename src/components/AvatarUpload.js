3b004f3dbd344b46e622ae18e89b144cce142be2
Duc Nguyen Van <ducnv@nal.vn>
2020-11-06 22:51:17 +0700

    [HRM90] add title

--- src/components/AvatarUpload.js
+++ src/components/AvatarUpload.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Upload } from 'antd';
-import ImgCrop from 'antd-img-crop';
 
 const AvatarUpload = () => {
   const [fileList, setFileList] = useState([]);
@@ -24,17 +23,15 @@ const AvatarUpload = () => {
   };
 
   return (
-    <ImgCrop rotate>
-      <Upload
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-        listType="picture-card"
-        fileList={fileList}
-        onChange={onChange}
-        onPreview={onPreview}
-      >
-        {fileList.length < 1 && '+ Avatar'}
-      </Upload>
-    </ImgCrop>
+    <Upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      listType="picture-card"
+      fileList={fileList}
+      onChange={onChange}
+      onPreview={onPreview}
+    >
+      {fileList.length < 1 && '+ Avatar'}
+    </Upload>
   );
 };
 
