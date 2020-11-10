01eb6ee7faa4c461889e93d4cd671c9abb91bd26
minhtuanha98 <minhtuanha98@gmail.com>
2020-11-10 10:29:44 +0700

    [HRM-64] Integrate skill list

--- src/components/Skill/SkillRegistInput.js
+++ src/components/Skill/SkillRegistInput.js
@@ -1,43 +1,124 @@
-import React, { useState } from 'react';
-import { Modal, Button, DatePicker, Form, Input } from 'antd';
-import '../../css/SkillRegistInput.css';
+import { Button, DatePicker, Form, Input, Modal } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { togglePopup } from '../../actions/utilsAction';
 import constant from '../../constants/htmlConstants';
+import { callApi } from '../../apis/axiosService';
+import '../../css/HolidayRegistPopup.css';
 
-function SkillRegistInput() {
-  const [visible, setVisible] = useState(false);
+function SkillRegistInput(props) {
+  const { active, value } = props;
+  const [skillDate, setSkillDate] = useState('');
+  const [name, setName] = useState('');
+  const [form] = Form.useForm();
+  const dispatch = useDispatch();
 
-  function showModal() {
-    setVisible(true);
-  }
-
-  function handleOk(event) {
-    setVisible(false);
-  }
-
-  function handleCancel() {
-    setVisible(false);
-  }
-
+  const handleChangeDatePicker = (date) => {
+    setSkillDate(date);
+  };
+  const handleChangeNote = (e) => {
+    setName(e.target.value);
+  };
+  const handleSaveAndQuit = () => {
+    form
+      .validateFields()
+      .then(() => {
+        callApi({
+          url: '/skills',
+          method: 'POST',
+          data: {
+            name,
+            updated_by: skillDate.format('YYYY-MM-DDT HH:mm:ss.sssZ'),
+          },
+        })
+          .then((res) => {
+            if (res.data !== null) {
+              form.resetFields();
+              dispatch(togglePopup());
+              setSkillDate('');
+              setName('');
+            }
+          })
+          .catch((err) => {
+            return err;
+          });
+      })
+      .catch((errorInfo) => {
+        return errorInfo;
+      });
+  };
+  const handleSaveAndContinue = () => {
+    form
+      .validateFields()
+      .then(() => {
+        callApi({
+          url: '/holidays',
+          method: 'POST',
+          data: {
+            date: skillDate.format('YYYY-MM-DD'),
+            name,
+            updated_by: skillDate.format('YYYY-MM-DDT HH:mm:ss.sssZ'),
+          },
+        })
+          .then((res) => {
+            if (res.data !== null) {
+              form.resetFields();
+              setSkillDate('');
+              setName('');
+            }
+          })
+          .catch((err) => {
+            return err;
+          });
+      })
+      .catch((errorInfo) => {
+        return errorInfo;
+      });
+  };
+  const handleCancel = () => {
+    form.resetFields();
+    setSkillDate('');
+    setName('');
+    dispatch(togglePopup());
+  };
   return (
-    <div className="skill-regist-input">
-      <div className="skill-regist-button">
-        <h2 className="list-title">Skill list</h2>
-        <Button type="primary" onClick={showModal}>
-          {constant.BUTTON.ADD}
-        </Button>
-      </div>
+    <div className="skill-regist-button">
       <Modal
-        title={constant.BUTTON.ADD}
-        visible={visible}
-        onOk={handleOk}
+        title={value === undefined ? 'Add New Skill' : 'Edit Skill'}
+        visible={active}
         onCancel={handleCancel}
-        okText="Save & Continue"
-        cancelText="Save & Close"
+        footer={
+          value === undefined
+            ? [
+                <Button htmlType="submit" onClick={handleSaveAndContinue}>
+                  {constant.BUTTON.SAVE_AND_CONTINUE}
+                </Button>,
+                <Button
+                  htmlType="submit"
+                  type="primary"
+                  onClick={handleSaveAndQuit}
+                >
+                  {constant.BUTTON.SAVE_AND_QUIT}
+                </Button>,
+              ]
+            : [
+                <Button
+                  htmlType="submit"
+                  type="primary"
+                  onClick={handleSaveAndQuit}
+                >
+                  {constant.BUTTON.SAVE_AND_QUIT}
+                </Button>,
+              ]
+        }
       >
         <Form
-          layout={constant.LAYOUT.HORIZONTAL}
-          labelCol={constant.LAYOUT_ERROR_TEXT.labelCol}
-          wrapperCol={constant.LAYOUT_ERROR_TEXT.wrapperCol}
+          layout="horizontal"
+          labelCol={{ span: constant.LABEL_COL.LABEL_COL_8 }}
+          wrapperCol={{ span: constant.WRAPPER_COL.WRAPPER_COL_12 }}
+          form={form}
+          onFinish={handleSaveAndQuit}
         >
           <Form.Item label={constant.LABEL.NAME}>
             <Input placeholder="Skill name" />
--- src/components/Skill/SkillSearchBox.js
+++ src/components/Skill/SkillSearchBox.js
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Input, Button, Select, DatePicker, Row, Col } from 'antd';
 import '../../css/SkillSearchBox.css';
+
+import { FormProvider } from 'antd/lib/form/context';
 import constant from '../../constants/htmlConstants';
 
-function SkillSearchBox() {
+function SkillSearchBox({ setCurrentName, setCurrentPage }) {
   const [componentSize, setComponentSize] = useState('default');
-  const [getInput, setGetInput] = useState('');
-
+  const [search, setSearch] = useState('');
+  const [form] = Form.useForm();
   const onFormLayoutChange = ({ size }) => {
     setComponentSize(size);
   };
   const handleChange = (e) => {
-    e.preventDefault();
+    setSearch(e.target.value);
   };
-  const handleSearch = () => {};
+  const handleSearch = () => {
+    // setCurrentPage(0);
+    setCurrentName(search);
+  };
+  const handleCancel = () => {
+    form.resetFields();
+    setCurrentName('');
+    setSearch('');
+  };
   return (
     <div className="search-box skill-search-box">
       <Row>
@@ -29,18 +39,14 @@ function SkillSearchBox() {
             }}
             onValuesChange={onFormLayoutChange}
             size={componentSize}
+            form={form}
           >
-            <Form.Item label={constant.LABEL.NAME}>
-              <Input
-                onChange={(e) => {
-                  handleChange(e);
-                }}
-                placeholder="Skill name"
-              />
+            <Form.Item name="search" label={constant.LABEL.NAME}>
+              <Input onChange={handleChange} placeholder="Skill name" />
             </Form.Item>
 
             <div className="user-search-box-button">
-              <Button>{constant.BUTTON.CANCEL}</Button>
+              <Button onClick={handleCancel}>{constant.BUTTON.CANCEL}</Button>
               <Button type="primary" onClick={handleSearch}>
                 {constant.BUTTON.SEARCH}
               </Button>
