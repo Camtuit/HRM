c5f71f301502c6834bd4a25ffc463f9cbe76fb4a
Duc Nguyen Van <ducnv@nal.vn>
2020-11-10 09:27:51 +0700

    [HRM68] fix login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button, Checkbox } from 'antd';
+
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 import '../css/Login.css';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [passWord, setPassWord] = useState('');
+  const [isPasswordCorrect, setIsPasswordCorrect] = useState(true);
+
+  const history = useHistory();
   const layout = {
     labelCol: {
       span: 8,
@@ -23,11 +31,58 @@ function Login() {
 
   const onFinishFailed = (errorInfo) => {};
 
+  // function onValue(changedValues, allValues) {
+  //   console.log(allValues, changedValues);
+  // }
+
+  // function onChange() {
+  //   if (isRemember === true) {
+  //     // localStorage.setItem('email', email);
+  //     // localStorage.setItem('passWord', passWord);
+  //     dispatch(forgetMe());
+  //   } else {
+  //     // localStorage.removeItem('email');
+  //     // localStorage.removeItem('passWord');
+
+  //     dispatch(rememberMe());
+  //   }
+  // }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    const data = {
+      email,
+      password: passWord,
+    };
+
+    axios
+      .post(' http://api-php.dev-hrm.nals.vn/api/auth/login', data)
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem('token', res.data.meta.access_token);
+        history.push('/users');
+      })
+      .catch((err) => {
+        setIsPasswordCorrect(false);
+        console.log(err);
+      });
+  }
   return (
     <div className="login">
       <div className="login-form">
         <h3>Human Resources Management</h3>
         <h1>NALS</h1>
+        {!isPasswordCorrect && (
+          <div className="login-caution">
+            <p>
+              Incorrect email or password.{' '}
+              <i
+                class="fas fa-times"
+                onClick={() => setIsPasswordCorrect(true)}
+              ></i>
+            </p>
+          </div>
+        )}
 
         <Form
           {...layout}
@@ -40,8 +95,6 @@ function Login() {
         >
           <Form.Item
             className="form-item"
-            // label="Email"
-
             name="email"
             rules={[
               {
@@ -55,11 +108,14 @@ function Login() {
               },
             ]}
           >
-            <Input placeholder="Email" />
+            <Input
+              placeholder="Email"
+              onChange={(event) => setEmail(event.target.value)}
+              defaultValue={localStorage.getItem('email')}
+            />
           </Form.Item>
 
           <Form.Item
-            // label="Password"
             className="form-item"
             name="password"
             rules={[
@@ -69,20 +125,23 @@ function Login() {
               },
             ]}
           >
-            <Input.Password placeholder="Password" />
+            <Input.Password
+              placeholder="Password"
+              onChange={(event) => setPassWord(event.target.value)}
+              defaultValue={localStorage.getItem('passWord')}
+            />
           </Form.Item>
 
           <Form.Item
             {...tailLayout}
             className="form-item-remember"
             name="remember"
-            valuePropName="checked"
           >
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
 
           <div className="login-form-button">
-            <Button type="primary" htmlType="submit">
+            <Button type="primary" htmlType="submit" onClick={onSubmit}>
               Login
             </Button>
           </div>
