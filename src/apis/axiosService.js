a4925b9a911a55ac4673a4274a8accaa55f801c2
Duc Nguyen Van <ducnv@nal.vn>
2020-11-10 13:02:46 +0700

    Fix conflict

--- src/apis/axiosService.js
+++ src/apis/axiosService.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 import { handleResponse, handleError } from '../helpers/apiHelper';
 
-<<<<<<< HEAD
-const baseURL = 'http://api-java.dev-hrm.nals.vn/api';
-=======
 const baseURL = process.env.API_URL || 'http://api-php.dev-hrm.nals.vn/api';
 const subBaseURL =
   process.env.API_URL_SUB || 'http://api-java.dev-hrm.nals.vn/api';
->>>>>>> feature/HRM-95-responsive-for-website
 
 export const instanceAxios = axios.create({
   baseURL,
