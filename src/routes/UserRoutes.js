a8c83fc49b33e0a170e2c3220fa3fbc61956231d
Duc Nguyen Van <ducnv@nal.vn>
2020-11-16 10:58:00 +0700

    [HRM120] fix private route

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,12 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import UserList from '../pages/User/UserList';
 import UserRegist from '../pages/User/UserRegist';
-
+import PrivateRoute from '../components/commons/PrivateRoute';
 
 export default function DeviceRoutes() {
   return (
     <Switch>
-      <Route exact path="/users" component={UserList} />
+      <PrivateRoute exact path="/users" component={UserList} />
       <Route exact path="/users/:id" component={UserRegist} />
     </Switch>
   );
