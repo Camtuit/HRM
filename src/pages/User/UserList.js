c734199049d2aefeff5a1d859d7c990a433ce232
hrm-fe <tuptc@nal.vn>
2020-11-21 14:48:45 +0700

    [HRM-224] Fix conflict

--- src/pages/User/UserList.js
+++ src/pages/User/UserList.js
@@ -5,19 +5,13 @@ import UserTable from '../../components/User/UserTable';
 import WrapperContent from '../../components/commons/WrapperContentPage';
 import { useTranslation } from 'react-i18next';
 import '../../css/UserList.css';
-
 function UserList() {
   const [fullName, setFullName] = useState('');
   const [contractStatus, setContractStatus] = useState('');
   const [contractDateBegin, setContractDateBegin] = useState('');
   const [contractDateEnd, setContractDateEnd] = useState('');
-<<<<<<< HEAD
   const [page, setPage] = useState(0);
   const { t, i18n } = useTranslation();
-=======
-  const [employeeStatus, setEmployeeStatus] = useState(1);
-  const [page, setPage] = useState(1);
->>>>>>> 14bd3de88fef9646fb8f75d4c9670e789fb1605c
   return (
     <WrapperContent className="user-list-content">
       <h2 className="list-title">{t('USER.USER_LIST')}</h2>
@@ -27,14 +21,14 @@ function UserList() {
         setContractStatus={setContractStatus}
         setContractDateBegin={setContractDateBegin}
         setContractDateEnd={setContractDateEnd}
-        setEmployeeStatus={setEmployeeStatus}
+        // setEmployeeStatus={setEmployeeStatus}
       />
       <UserTable
         fullName={fullName}
         contractStatus={contractStatus}
         contractDateBegin={contractDateBegin}
         contractDateEnd={contractDateEnd}
-        employeeStatus={employeeStatus}
+        // employeeStatus={employeeStatus}
         page={page}
         setPage={setPage}
         setFullName={setFullName}
