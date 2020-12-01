492b8a190c0373d2219513538a608ebf04655cd3
minhtuanha98 <minhtuanha98@gmail.com>
2020-12-01 14:08:40 +0700

    [HRM-268] Fix conflicted profile button

--- src/components/Profile/ProfileButton.js
+++ src/components/Profile/ProfileButton.js
@@ -1,32 +1,39 @@
 import '../../assets/scss/profile/index.scss';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import React, { useState } from 'react';
 import { Button, Col, Row } from 'antd';
 
 export default function ProfileButton() {
+  const { t, i18n } = useTranslation();
   return (
-    <Row gutter={[16, 16]} className="row-bnt">
-      <Col xs={{ span: 4, offset: 1 }} lg={{ span: 2, offset: 2 }}>
-        <Link to="/holidays">
-          <Button type="primary" className="btn-profile">
-            List day Off
-          </Button>
-        </Link>
-      </Col>
-      <Col xs={{ span: 4, offset: 1 }} lg={{ span: 2, offset: 2 }}>
-        <Link to="/request-devices">
-          <Button type="primary" className="btn-profile">
-            Request Device
-          </Button>
-        </Link>
-      </Col>
-      <Col xs={{ span: 4, offset: 2 }} lg={{ span: 2, offset: 2 }}>
-        <Link to="/dayoff">
-          <Button type="primary" className="btn-profile">
-            Submit Day Off
-          </Button>
-        </Link>
-      </Col>
-    </Row>
+    <div className="profile-button">
+      <Row className="row-bnt">
+        <Col xs={0} sm={0} md={4} lg={4} xl={4}>
+          {' '}
+        </Col>
+        <Col xs={8} sm={8} md={6} lg={6} xl={4}>
+          <Link to="/holidays">
+            <Button type="primary" className="btn-profile">
+              {t('profile.list_day_off')}
+            </Button>
+          </Link>
+        </Col>
+        <Col xs={8} sm={8} md={6} lg={6} xl={4}>
+          <Link to="/request-devices">
+            <Button type="primary" className="btn-profile">
+              {t('profile.request_device')}
+            </Button>
+          </Link>
+        </Col>
+        <Col xs={8} sm={8} md={6} lg={6} xl={4}>
+          <Link to="/dayoffs">
+            <Button type="primary" className="btn-profile">
+              {t('profile.submit_day_off')}
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+    </div>
   );
 }
